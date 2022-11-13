526795ffd8cfc170402b65509057a6926f5476d9
gretel22 <gmartinez@trilogyed.com>
2022-11-13 10:52:36 -0500

    Social links - href fixed

--- src/App.tsx
+++ src/App.tsx
@@ -39,10 +39,10 @@ export default function Example() {
         <div className="relative px-6 lg:px-8">
           <div className='relative flex justify-center'>
             <div className='absolute top-10 border mr-16 rounded-full w-fit p-2 border-2 cursor-pointer border-violet-600 hover:bg-opacity-25 hover:bg-violet-300'>
-              <a target="_blank" href='mailto:gretelmn@gmail.com' rel="noreferrer"><img src="./assets/linkedin.png" alt='linkedin-icon'/></a>
+              <a target="_blank" href='https://www.linkedin.com/in/gretelmn/' rel="noreferrer"><img src="./assets/linkedin.png" alt='linkedin-icon'/></a>
             </div>
             <div className='absolute top-10 border ml-16 rounded-full w-fit p-2 border-2 cursor-pointer border-violet-600 hover:bg-opacity-25 hover:bg-violet-300'>
-              <a target="_blank" href='https://www.linkedin.com/in/gretelmn/' rel="noreferrer"><img src="./assets/email.png" alt='email-icon'/></a>
+              <a target="_blank" href='mailto:gretelmn@gmail.com' rel="noreferrer"><img src="./assets/email.png" alt='email-icon'/></a>
             </div>
           </div>
           <div className="mx-auto max-w-3xl pt-10 pb-10 sm:pt-48 sm:pb-40">
@@ -59,13 +59,11 @@ export default function Example() {
                 <p className="text-lg leading-8 text-gray-600 sm:text-center">
                   Meantime please check my repositories in <span className='font-bold'>Github</span>:
                 </p>
-
                 <div className="mt-8 mx-auto flex sm:justify-center border-2 border-white w-fit rounded-full p-2 pr-3 hover:border-violet-600">                
                   <a target="_blank" href='https://github.com/grety22?tab=repositories' rel="noreferrer">
                     <img src="./assets/github.png" alt='github-icon'/>
                   </a>
                 </div>
-
                 <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
                   Thank you for your patience. 
                 </p>
@@ -73,7 +71,6 @@ export default function Example() {
                   I'm looking forward to show you my career journey.
                 </p>
               </div>
-
               <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
                 <svg
                   className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
@@ -101,11 +98,7 @@ export default function Example() {
                   </defs>
                 </svg>
               </div>
-
-
-
               <Animation />
-
           </div>
         </div>
       </main>
