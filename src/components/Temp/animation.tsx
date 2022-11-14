96f704310a82527c9ce26b0e507db8115d68a589
grety22 <23240307+grety22@users.noreply.github.com>
2022-11-14 00:41:33 -0500

    Breakpoint - fixes

--- src/components/Temp/animation.tsx
+++ src/components/Temp/animation.tsx
@@ -2,7 +2,7 @@ import Lottie from "lottie-react";
 import waiting from "./waiting.json";
 
 const Animation = () => {
-  return <Lottie className='w-100 right-2 absolute -bottom-40' animationData={waiting} />;
-};
+  return <Lottie className='h-auto absolute -right-10 bottom-0 hidden min-[320px]:w-60 min-[320px]:block sm:w-72 md:w-96 lg:w-[420px] min-[1200px]:w-fit' animationData={waiting} />;
+}
 
 export default Animation;
